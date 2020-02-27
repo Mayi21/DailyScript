f35f86223788b30810d78c76979dd1df677daf35
Mayi21 <May_i21@163.com>
2020-02-27 15:53:18 +0800

    fix problem

--- PythonEnv.sh
+++ PythonEnv.sh
@@ -6,7 +6,8 @@ yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
 cd /root/env/
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tar.xz
-tar -zxjf  Python-3.7.6.tar.xz
+xz -d Python-3.7.6.tar.xz
+tar xvf  Python-3.7.6.tar
 cd Python-3.7.6
 ./configure prefix=/usr/local/python3/
 make && make install
