49fe526aea66c2ff056064e098c2d081735cb21f
Mayi21 <May_i21@163.com>
2020-02-28 16:31:57 +0800

    Update PythonEnv.sh

--- PythonEnv.sh
+++ PythonEnv.sh
@@ -3,7 +3,10 @@ if [ ! -d "/root/env" ];then
         mkdir /root/env/
 fi
 yum -y groupinstall "Development tools"
-yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
+yum -y install zlib-devel bzip2-devel openssl-devel 
+yum -y install ncurses-devel sqlite-devel readline-devel 
+yum -y install tk-devel gdbm-devel db4-devel libpcap-devel 
+yum -y install xz-devel libffi-devel vim
 cd /root
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tar.xz
 xz -d Python-3.7.6.tar.xz
@@ -28,13 +31,12 @@ if [ -f "/usr/bin/python" ];then
     fi
 fi
 ln -s /root/env/python3/bin/python3 /usr/bin/python
-source='#!/usr/bin/python'
-target='#!/usr/bin/python2'
-sed -i 's/${source}/${target}' /usr/bin/yum
-sed -i 's/${source}/${target}' /usr/libexec/urlgrabber-ext-down
+sed -i '1c #!/usr/bin/python2' /usr/bin/yum
+sed -i '1c #!/usr/bin/python2' /usr/libexec/urlgrabber-ext-down
 if [ -f "/usr/bin/pip" ];then
     rm -rf /usr/bin/pip
 fi
 ln -s /root/env/python3/bin/pip3 /usr/bin/pip
 echo 'installed complete'
 python -V
+
