0fbe40a78dfc4594a5893ec0b1a4cbf713bbb782
Mayi21 <May_i21@163.com>
2020-03-01 22:30:11 +0800

    Update JavaEnv.sh

--- JavaEnv.sh
+++ JavaEnv.sh
@@ -4,8 +4,7 @@ if [ ! -d "/root/env" ];then
         mkdir /root/env/
 fi
 cd /root/env/
-# 安装包问题还没有解决CC
-# wget "https://download.oracle.com/otn/java/jdk/8u241-b07/1f5b5a70bf22433b84d0e960903adac8/jdk-8u241-linux-x64.tar.gz"
+wget http://39.105.163.148/nextcloud/remote.php/webdav/jdk-8u181-linux-x64.tar.gz
 tar -zxvf jdk-8u181-linux-x64.tar.gz
 mv jdk1.8.0_181 jdk
 rm -rf jdk-8u181-linux-x64.tar.gz
