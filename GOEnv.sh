936eea5117b458fb255c7e8aef2a549181788c5b
Mayi21 <May_i21@163.com>
2021-03-09 20:50:46 +0800

    delete

--- GOEnv.sh
+++
@@ -1,8 +0,0 @@
-# linux安装GO
-yum -y update
-yum -y install wget
-mkdir -p /root/env/go && cd /root/env/go
-wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
-tar -zxvf 
-export PATH=$PATH:/root/env/go/go/bin
-go version
